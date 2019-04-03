5669731eee6bb20abf98cd844a8aaa2a0a839cd1
Chris Smith <chris@hichris.com>
2019-04-03 19:06:07 -0400

    Refactored code moving read+write functions into separate functions. tabs > spaces

--- src/main.rs
+++ src/main.rs
@@ -1,31 +1,52 @@
 use std::io::{Seek, SeekFrom, Read, Write};
 use std::env;
-use std::fs::OpenOptions;
+use std::fs::{OpenOptions, File};
 use std::sync::Mutex;
 
 mod buse;
 
+struct Source {
+	file: File,
+	size: u64,
+}
+
 fn main() -> Result<(), ()> {
-    let device = env::args().nth(1).expect("Expected device parameter");
-    println!("device: {}", device);
+	let device = env::args().nth(1).expect("Expected device parameter");
+	println!("device: {}", device);
+	
+	let sources: Vec<Source> = env::args()
+		.skip(2) // skip the binary path and device parameter
+		.map(|path| {
+			println!("source: {}", path);
+			let file = OpenOptions::new().read(true).write(true).open(path).unwrap();
+			let size = file.metadata().unwrap().len();
+			
+			Source { file, size }
+		})
+		.collect();
+	
+	let size = sources.iter().fold(0, |acc, val| acc.max(val.size));
+	println!("size: {}", size);
+	
+	let sources = Mutex::new(sources);
+	
+	buse::main(device, size,
+			   |buf, offset| read(&sources.lock().unwrap(), buf, offset),
+			   |buf, offset| write(&sources.lock().unwrap(), buf, offset))
+}
 
-    let source = env::args().nth(2).expect("Expected source file");
-    println!("source: {}", source);
+fn read(sources: &Vec<Source>, buf: &mut [u8], offset: u64) -> Result<(), ()> {
+	let mut source = &sources[0].file;
+	
+	source.seek(SeekFrom::Start(offset)).unwrap();
+	source.read_exact(buf).unwrap();
+	Ok(())
+}
 
-    let source_file = OpenOptions::new().read(true).write(true).open(source).unwrap();
-    let size = source_file.metadata().unwrap().len();
-
-    let source_file = Mutex::new(source_file);
-
-    buse::main(device, size, |buf, offset| -> Result<(), ()> {
-        let mut source_file = source_file.lock().unwrap();
-        source_file.seek(SeekFrom::Start(offset)).unwrap();
-        source_file.read_exact(buf).unwrap();
-        Ok(())
-    }, |buf, offset| -> Result<(), ()> {
-        let mut source_file = source_file.lock().unwrap();
-        source_file.seek(SeekFrom::Start(offset)).unwrap();
-        source_file.write_all(buf).unwrap();
-        Ok(())
-    })
+fn write(sources: &Vec<Source>, buf: &[u8], offset: u64) -> Result<(), ()> {
+	let mut source = &sources[0].file;
+	
+	source.seek(SeekFrom::Start(offset)).unwrap();
+	source.write_all(buf).unwrap();
+	Ok(())
 }
